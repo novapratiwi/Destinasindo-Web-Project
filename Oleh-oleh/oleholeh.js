436ce76ab6fdb1b1254c78969c5f4df63459b235
novapratiwi <nova.iramma007@gmail.com>
2021-01-14 17:27:01 +0800

    klik bintang

---
+++ Oleh-oleh/oleholeh.js
@@ -0,0 +1,15 @@
+function star1(){
+    document.getElementById("bintang1").src = '../star.png';
+}
+
+function star2(){
+    document.getElementById("bintang2").src = '../star.png';
+}
+
+function star3(){
+    document.getElementById("bintang3").src = '../star.png';
+}
+
+function star4(){
+    document.getElementById("bintang4").src = '../star.png';
+}
